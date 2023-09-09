6048dce34f51d377580cfba2960f798f2b4639fe
MatIvan <mativ.rufous@yandex.ru>
2023-09-09 10:00:25 +0300

    add base reaction on bot's name

--- katya/katya.js
+++ katya/katya.js
@@ -17,10 +17,31 @@ function start() {
  * @param { TelegramBot.Metadata}metadata
  */
 function onMessage(msg, metadata) {
+    const { id, username } = msg.chat;
     console.debug(msg, metadata);
-    console.log(msg.chat.title + " : " + msg.text);
+    console.log(username + " : " + msg.text);
+    if (!msg.text) {
+        return;
+    }
+    if (hasKatyaName(msg.text)) {
+        bot.sendMessage(id, 'Я Катя!');
+    }
     //const chatId = msg.chat.id;
     //bot.sendMessage(chatId, 'Received your message');
+}
+
+const KATYA_NAMES = ["КАТЯ", "КАТЕ", "КАТЮ", "КАТЕЙ"]
+/**
+ * @param {string} txt 
+ */
+function hasKatyaName(txt) {
+    const upperText = txt.toUpperCase();
+    for (const name of KATYA_NAMES) {
+        if (upperText.indexOf(name) > -1) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = {
