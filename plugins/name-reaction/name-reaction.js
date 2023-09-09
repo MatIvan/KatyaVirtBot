b1adc6b595ad73088072d952ba1844d85269bb70
MatIvan <mativ.rufous@yandex.ru>
2023-09-09 19:39:32 +0300

    refactoring plugin

--- plugins/name-reaction/name-reaction.js
+++ plugins/name-reaction/name-reaction.js
@@ -1,40 +1,12 @@
-//@ts-check
-const express = require('express');
-
-/**
- * @typedef {object} Body
- * @property {string} message
- * @property {Hook} hook
- */
-
-/**
- * @typedef {object} Hook
- * @property {number} id
- * @property {number} userId
- * @property {string} url
- * @property {Condition} condition
- */
-
-/**
- * @typedef {object} Condition
- * @property {string} chatName
- * @property {string[] | undefined} contains 
- * @property {string[] | undefined} startWith
- * @property {boolean} caseSensitive
- */
-
 const port = 11001;
-const katyaServerUrl = 'http://katya:8888';
+const katyaServerUrl = 'http://katya:8888/send';
 const userToken = 'DEMOTOKEN';
 
+const express = require('express');
 const app = express();
 app.use(express.json());
 app.post("/", (req, resp) => {
-    console.log("request");
-    onHook(req.body)
-        .then(() => { console.log("ok") })
-        .catch(() => { console.log("error") })
-    console.log("end");
+    onHook(req.body);
     resp.end();
 })
 
@@ -42,13 +14,8 @@ app.listen(port, function () {
     console.info('Server start on port ' + port);
 });
 
-
-/**
- * @param {Body} request
- */
 function onHook(request) {
     const { hook, message } = request;
-    console.log('Sending. hookId=' + hook.id);
     const body = {
         chatName: hook.condition.chatName,
         message: 'Я Катя!'
@@ -60,12 +27,5 @@ function onHook(request) {
             'Content-Type': 'application/json',
             'Authorization': 'Token ' + userToken
         },
-    })
-        .then(resp => {
-            console.log(`Sended. hookId=${hook.id} status=${resp.status}`);
-        })
-        .catch(e => {
-            console.error(e);
-            console.log(`Sending error. hookId=${hook.id} error=${e}`);
-        })
+    });
 }
